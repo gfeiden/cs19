b9dd0d3ec5ffd2db3b6f8e38b3032c5a861b381a
gfeiden <gregory.a.feiden.gr@dartmouth.edu>
2014-10-17 17:56:51 +0200

    start of abstract form

---
+++ js/abstract.js
@@ -0,0 +1,7 @@
+/* Handles formatting issues related to abstract submission */
+
+
+function abstractPreview(input) {
+    // format abstract input with MathJax and display in preview frame
+    
+}
