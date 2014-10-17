0c8ba008985b49d3d8a635c584a154650d05e23f
gfeiden <gregory.a.feiden.gr@dartmouth.edu>
2014-10-18 00:52:27 +0200

    preview with mathjax

--- js/abstract.js
+++ js/abstract.js
@@ -1,7 +1,30 @@
 /* Handles formatting issues related to abstract submission */
 
 
-function abstractPreview(input) {
+function abstractPreview() {
     // format abstract input with MathJax and display in preview frame
+    input = document.getElementById("abstract");
+    output = document.getElementById("abstractPreview");
+    
+    // create preview section
+    output.style.color = "black";
+    output.innerHTML = input.value;
+    
+    // typeset mathematics with MathJax
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,"abstractPreview"]);
+    
+}
+
+function abstractReset() {
+    // reset abstract input 
+    input = document.getElementById("abstract");
+    output = document.getElementById("abstractPreview");
+    
+    // create preview section
+    input.value = "";
+    output.innerHTML = "";
+    
+    // typeset mathematics with MathJax
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,"abstractPreview"]);
     
 }
