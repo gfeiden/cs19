332b9e974abc3464468fa76819a6dfa30962901f
gfeiden <gregory.a.feiden.gr@dartmouth.edu>
2014-10-18 19:37:18 +0200

    abstract form improved

--- js/abstract.js
+++ js/abstract.js
@@ -4,15 +4,35 @@
 function abstractPreview() {
     // format abstract input with MathJax and display in preview frame
     input = document.getElementById("abstract");
-    output = document.getElementById("abstractPreview");
+    frame = document.getElementById("preview");
+    abtext = document.getElementById("abstractPreview");
+    author = document.getElementById("authorPreview");
+    coauth = document.getElementById("coauthorPreview");
+    title = document.getElementById("titlePreview");
+        
+    // format abstract section
+    frame.style.color = "black";
+    frame.style.backgroundColor = "#eeeeee";
+    frame.style.padding = "5px 10px 10px 10px";
+    frame.style.borderRadius = "5px";
+      
+    // format title
+    title.style.fontWeight = "normal";
+    title.innerHTML = document.getElementById("title").value;
+      
+    // create author list preview
+    author.style.fontWeight = "bold";
+    author.innerHTML = document.getElementById("firstauthor").value;
     
-    // create preview section
-    output.style.color = "black";
-    output.innerHTML = input.value;
+    // create co-author list
+    coauth.innerHTML = document.getElementById("coauthors").value;
+    
+    // create abstract preview
+    abtext.innerHTML = input.value;
+    abtext.style.textAlign = "justify";
     
     // typeset mathematics with MathJax
     MathJax.Hub.Queue(["Typeset",MathJax.Hub,"abstractPreview"]);
-    
 }
 
 function abstractReset() {
@@ -23,6 +43,8 @@ function abstractReset() {
     // create preview section
     input.value = "";
     output.innerHTML = "";
+    output.style.backgroundColor = "#eeeeee";
+    output.style.padding = "0px";
     
     // typeset mathematics with MathJax
     MathJax.Hub.Queue(["Typeset",MathJax.Hub,"abstractPreview"]);
